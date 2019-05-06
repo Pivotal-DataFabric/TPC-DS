534fc0c8cb8bc38cdabf789cb1e59860e8d4100d
Jason Vigil <jvigil@pivotal.io>
2019-05-06 10:14:18 -0600

    Correct the variable positions for 09_score/rollout.sh
    Authored-by: Jason Vigil <jvigil@pivotal.io>

--- 09_score/rollout.sh
+++ 09_score/rollout.sh
@@ -7,9 +7,9 @@ source_bashrc
 
 GEN_DATA_SCALE=$1
 EXPLAIN_ANALYZE=$2
-RANDOM_DISTRIBUTION=$3
-MULTI_USER_COUNT=$4
-SINGLE_USER_ITERATIONS=$5
+RANDOM_DISTRIBUTION=$4
+MULTI_USER_COUNT=$5
+SINGLE_USER_ITERATIONS=1
 
 if [[ "$GEN_DATA_SCALE" == "" || "$EXPLAIN_ANALYZE" == "" || "$RANDOM_DISTRIBUTION" == "" || "$MULTI_USER_COUNT" == "" || "$SINGLE_USER_ITERATIONS" == "" ]]; then
 	echo "You must provide the scale as a parameter in terms of Gigabytes, true/false to run queries with EXPLAIN ANALYZE option, true/false to use random distrbution, multi-user count, and the number of sql iterations."
